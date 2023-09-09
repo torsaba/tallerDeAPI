4fa68f3a4543e806118636c74abb10f10afe5a29
torsaba <lucia.bperez.a@gmail.com>
2023-09-08 22:31:35 -0300

    Terminado
    Lo pude terminar!!

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-const API_URL = "https://jsonplaceholder.typicode.com/users";
 
-fetch(API_URL)
-    .then(response => response.json)
-    .then(data => console.log(data))
+const API_URL = "https://jsonplaceholder.typicode.com/users";
+const form = document.getElementById("form");
+
+
+const saveForm = form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById("name");
+    const surname = document.getElementById("surname");
+    const date = document.getElementById("date");
+
+    const info = {
+        name: name.value,
+        surname: surname.value,
+        date: date.value
+    };
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+        },
+        body: JSON.stringify(info),
+    };
+
+    fetch(API_URL, options)
+        .then(data => {
+            if (!data.ok) {
+                throw Error(data.status);
+            }
+            return data.json();
+        })
+        .then(info => {
+            console.log(info);
+        })
+        .catch(e => console.log(e));
+});
+
+
